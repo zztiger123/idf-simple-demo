8153b55d74462c1a47d381898df04272c73d4a60
Zheng Zhong <zhengzhong@espressif.com>
2020-12-24 21:07:35 +0800

    simplify the code

--- example/protocols/http_request_static_ip/main/http_request_example_main.c
+++ example/protocols/http_request_static_ip/main/http_request_example_main.c
@@ -9,18 +9,20 @@
 #include <string.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "freertos/event_groups.h"
 #include "esp_system.h"
 #include "esp_wifi.h"
 #include "esp_event.h"
 #include "esp_log.h"
 #include "nvs_flash.h"
-#include "protocol_examples_common.h"
 
 #include "lwip/err.h"
 #include "lwip/sockets.h"
 #include "lwip/sys.h"
 #include "lwip/netdb.h"
 #include "lwip/dns.h"
+
+#include "station.h"
 
 /* Constants that aren't configurable in menuconfig */
 #define WEB_SERVER "www.baidu.com"
@@ -122,14 +124,7 @@ static void http_get_task(void *pvParameters)
 void app_main(void)
 {
     ESP_ERROR_CHECK( nvs_flash_init() );
-    ESP_ERROR_CHECK(esp_netif_init());
-    ESP_ERROR_CHECK(esp_event_loop_create_default());
-
-    /* This helper function configures Wi-Fi or Ethernet, as selected in menuconfig.
-     * Read "Establishing Wi-Fi or Ethernet Connection" section in
-     * examples/protocols/README.md for more information about this function.
-     */
-    ESP_ERROR_CHECK(example_connect());
+    wifi_init_sta();
 
     xTaskCreate(&http_get_task, "http_get_task", 4096, NULL, 5, NULL);
 }
