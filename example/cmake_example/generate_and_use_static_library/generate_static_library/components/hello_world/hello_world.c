7af63aab72a3fab8744ab388076ad2c52a30607e
Zheng Zhong <zhengzhong@espressif.com>
2021-11-26 15:09:28 +0800

    add base code

---
+++ example/cmake_example/generate_and_use_static_library/generate_static_library/components/hello_world/hello_world.c
@@ -0,0 +1,17 @@
+/* Hello World Example
+
+   This example code is in the Public Domain (or CC0 licensed, at your option.)
+
+   Unless required by applicable law or agreed to in writing, this
+   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied.
+*/
+#include <stdio.h>
+
+#include "hello_world.h"
+
+void printf_hello_world(void)
+{
+    printf("Hello world!\n");
+
+}
